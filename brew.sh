ed7061d27d1adee188c57eaefc233dcf70feace1
Yondonrinchin Batsukh <yondon898@gmail.com>
2022-01-14 15:10:30 +0800

    .

---
+++ brew.sh
@@ -0,0 +1,33 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+
+brew install dockutil
+brew install iftop
+brew install htop
+brew install git
+brew install wget
+brew install reattach-to-user-namespace
+brew install awscli
+brew install gnupg
+brew install jpeg
+brew install jq
+brew install kubernetes-cli
+brew install kustomize
+brew install terraform
+brew install telnet
+brew install tmux
+brew install tree
+brew install tree-sitter
+brew install minikube
+brew install nmap
+brew install cmake
+brew install erlang
+brew install go
+brew install fontconfig
+brew install brotli
+brew install imagemagick
+brew install vault
+
+echo "Success! Brew addy apps are installed."
+
